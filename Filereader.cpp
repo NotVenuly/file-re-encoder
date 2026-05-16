302e8f98d0456eb22485fdb0eb00ca65cbd11fcb
NotVenuly <147390259+NotVenuly@users.noreply.github.com>
2026-05-17 00:39:04 +0300

    files open correctly

--- Filereader.cpp
+++ Filereader.cpp
@@ -3,16 +3,35 @@
 #include <string>
 using namespace std;
 
+void Trim(string* address, int index){
+    int length = (*address).length()-1;
+    if (index != length){
+        for (int i=index; i<length; i++){
+            (*address)[i] = (*address)[i+1];
+        }
+        address->pop_back();
+    }
+    else{
+        address->pop_back();
+    }
+}
 
-int main(){
-
+void OpenFile(){
     string fileAddress;
     
     cout << "Type the address of the file you want to re-encode: ";
     getline(cin, fileAddress);
+    if(fileAddress[0] == '"'){
+
+        Trim(&fileAddress, 0);
+        Trim(&fileAddress, fileAddress.length()-1);
+    }
     ifstream myFile;
     myFile.open(fileAddress);
-    cout <<"File name: "<< myFile.is_open()<<endl;
+    if(myFile.is_open() != 1){
+        cout << "Something went wrong with opening the file" << endl;
+        OpenFile();
+    }
 
     if(myFile.is_open()){
         string line;
@@ -24,18 +43,9 @@ int main(){
         
     }
     myFile.close();
-    return 0;
 }
 
-void Trim(string* address, int index){
-    int length = (*address).length();
-    if (index != length){
-        for (int i=index; i<length; i++){
-            (*address)[i] = (*address)[i+1];
-        }
-        address->pop_back();
-    }
-    else{
-        address->pop_back();
-    }
+int main(){
+    OpenFile();
+    return 0;
 }
