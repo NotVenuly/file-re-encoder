2ff3eaedb2d9309c3824eeea0ed97b978e114c55
NotVenuly <147390259+NotVenuly@users.noreply.github.com>
2026-05-18 10:38:37 +0300

    added logic to file transformer

--- Filetransformer.cpp
+++ Filetransformer.cpp
@@ -1,10 +1,24 @@
-#include <iostream>
 #include "Filereader.h"
+#include <iostream>
+#include <list>
+#include <string>
+#include <vector>
+
 
 using namespace std;
 
-
-
-string TransformFile(string fileContent){
-
+string TransformFile(string fileContent) {
+  vector<int> lineEnd;
+  string currString;
+  list<string> allLines;
+  int lineCount;
+  for (int i = 0; i < fileContent.length() - 1; i++) {
+    currString = currString + fileContent[i];
+    if (fileContent[i] == '\n') {
+      allLines.push_back(currString);
+      currString = "";
+      lineEnd.push_back(i);
+      lineCount++;
+    }
+  }
 }
